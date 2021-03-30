7768fefb9fefcb7c4edb87e12488c9fbd709a847
gholiveira29 <g.h.p.o3615@gmail.com>
2021-03-30 17:29:14 -0300

    api

---
+++ api-server/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const Product = require('./product');
+
+var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+mongoose.connect('mongodb://localhost:27017/http_client',{useNewUrlParser: true});
+
+var myLogger = function(req,res,next) {
+    console.log(req.body);
+    next();
+}
+app.use(myLogger);
+
+app.get('/products', function(req,res) {
+    Product.find().lean().exec((err,prods) => {
+        if(err) {
+            res.status(500).send(err);
+        } else {
+            res.status(200).send(prods)
+        }
+    });
+});
+
+app.get('/productserr', function(req,res) {
+    setTimeout(() => {
+        res.status(500).send({
+            msg: "Error nessage from the server!"
+        });
+    }, 2000)
+});
+
+app.listen(3000);
