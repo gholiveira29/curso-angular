4e8cc6f741076cc68916c25334ae71b07fb5618f
gholiveira29 <g.h.p.o3615@gmail.com>
2021-04-07 19:10:08 -0300

    iniciando modulo 11

--- modulo-11/template-driven/src/app/app.module.ts
+++ modulo-11/template-driven/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { MaterialModule } from './material.module';
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplateDrivenFormComponent
   ],
   imports: [
     BrowserModule,
