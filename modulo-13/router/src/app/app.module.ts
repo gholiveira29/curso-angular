f97c23af182def00f0630a6872ea8547cfe70e4e
gholiveira29 <g.h.p.o3615@gmail.com>
2021-05-10 18:12:35 -0300

    criando projeto do modulo-13

---
+++ modulo-13/router/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BookComponent } from './book/book.component';
+import { DvdComponent } from './dvd/dvd.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BookComponent,
+    DvdComponent,
+    PageNotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
