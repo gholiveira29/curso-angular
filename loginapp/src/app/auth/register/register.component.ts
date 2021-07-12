ea8bc17a411a2f6a5780880cd16e3e572f1766e5
gholiveira29 <g.h.p.o3615@gmail.com>
2021-07-12 19:13:51 -0300

    criando formulario de registro

--- loginapp/src/app/auth/register/register.component.ts
+++ loginapp/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  formRegister = this.fb.group({
+    'firstname': ['', Validators.required],
+    'lastname': ['', Validators.required],
+    'address': ['', Validators.required],
+    'city': ['', Validators.required],
+    'state': ['', Validators.required],
+    'phone': ['', Validators.required],
+    'mobilephone': ['', Validators.required],
+    'email': ['',[ Validators.required, Validators.email]],
+    'password1': ['',[ Validators.required, Validators.minLength(6)]],
+    'password2': ['', [Validators.required, Validators.minLength(6)]],
+  }, { validator: this.matchingPasswords });
+
+  states = ['MG','RJ','SP','GO','PR']
+
+
+  constructor( 
+    private fb: FormBuilder
+    ) { }
+
+    matchingPasswords(group: FormGroup) {
+      if(group) {
+        const password1 = group.controls['password1'].value;
+        const password2 = group.controls['password2'].value;
+        if(password1 == password2) {
+          return null;
+        }
+      }
+      return {matching: false};
+    }
 
   ngOnInit(): void {
   }
+
+
+  onSubmit() {}
 
 }
